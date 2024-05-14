cef9f97403674aa1d346a8ffc67dda9510b4f5b6
lushij <118508477+lushij@users.noreply.github.com>
2024-05-14 15:30:59 +0800

    Delete cloudDisk1.0 directory

--- cloudDisk1.0/rabbitmq.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <string>
-struct RabbitMqInfo{
-    std::string RabbitURL = "amqp://guest:guest@127.0.0.1:5672";
-    std::string TransExchangeName = "uploadserver.trans";
-    std::string TransQueueName = "uploadserver.trans.oss";
-    std::string TransRoutingKey = "oss";
-};
-
